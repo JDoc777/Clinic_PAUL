922a10bb2ea51fae19310f18873f4a0e0ac9bc2f
Allent24 <allent24@students.rowan.edu>
2026-02-05 14:54:50 -0500

    Talking to PAUL
    Added a code where you say PAUL, where a menu pops up, and you can decide on what command to say next. Including pick up or turn around.

--- Raspberry PI/Voice_IO/speechToText_test.py
+++ Raspberry PI/Voice_IO/speechToText_test.py
@@ -1,6 +1,8 @@
 import speech_recognition as sr
 r = sr.Recognizer()
 
+menu_active = False
+
 while True:
     try:
         with sr.Microphone() as source:
@@ -8,13 +10,38 @@ while True:
             
             r.adjust_for_ambient_noise(source, duration=0.2)
             audio = r.listen(source)
-            text = r.recognize_google(audio)
-            text = text.lower()  
+            text = r.recognize_google(audio) 
+            text = text.lower()
             print("You said:", text)
             
-            if "exit" in text:
-                print("Exiting program...")
-                break
+            
+            if menu_active:
+                match text:
+                    case "turn around":
+                        print("Paul Turns Around")
+                        menu_active = False
+                    case "pick":
+                        print("Paul Picks")
+                        menu_active = False
+                    case "move forward":
+                        print("Paul Moves Forward")
+                        menu_active = False
+                    case "exit menu":
+                        menu_active = False
+                        print("Menu exited")
+                    case _:
+                        print("Unknown command. Menu: turn around, pick, move forward, exit menu")
+            else:
+                if "exit" in text:
+                    print("Exiting program...")
+                    break
+                if "quit" in text:
+                    print("Exiting program...")
+                    break
+                if "paul" in text:
+                    menu_active = True
+                    print("Menu activated. Say: turn around, pick, move forward, exit menu")
+                    break
 
     except sr.RequestError as e:
         print("Could not request results; {0}".format(e))
