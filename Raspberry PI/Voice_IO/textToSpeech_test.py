e9193d00c6c6b453f6b8e72e56d6a23660c0ee77
Paul_RaspberryPi4 <Henryayeke29344@gmail.com>
2026-02-10 14:58:15 -0500

    updated testtospeech_test.py
    tested using flite library, is "good enough" unless Piper begins to function.

--- Raspberry PI/Voice_IO/textToSpeech_test.py
+++ Raspberry PI/Voice_IO/textToSpeech_test.py
@@ -28,9 +28,9 @@
 #         time.sleep(delay)
 #         subprocess.run([
 #             "espeak-ng",
-#             "-v", "en-us+m1+sing", #Type of voice
-#             "-a", "200", #Amplitude (0-200)
-#             "-p", "55", #Pitch (0-99)
+#             "-v","en-us+f3", #Type of voice
+#             "-a", "100", #Amplitude (0-200)
+#             "-p", "30", #Pitch (0-99)
 #             "-s", "140", #Speed (words per minute)
 #             "-k", "40", #Word gap (0-99)
 #             text
@@ -46,7 +46,7 @@
 
 # # Enqueue lines (POP order)
 # lines = [
-#     ("Hello I am PAUL the programmable autonomous utility lift. I am your personal wink wink assistant", 0.1),
+#     ("Hello I am PAUL ", 0.1),
     
 # ]
 
@@ -62,67 +62,71 @@
 
 # print("Done Speaking")
 
-import subprocess
-import threading
-import time
-import tempfile
-import os
-from queue import Queue
+# import subprocess
+# import threading
+# import time
+# import tempfile
+# import os
+# from queue import Queue
 
-PIPER_MODEL = "/home/justina/piper-voices/en_US-jenny-medium.onnx"  # <-- change this
+# PIPER_MODEL = "/home/justina/piper-voices/en_US-jenny-medium.onnx"  # <-- change this
 
-def speak_piper(text: str):
-    # Make a temp wav file
-    fd, wav_path = tempfile.mkstemp(suffix=".wav")
-    os.close(fd)
+# def speak_piper(text: str):
+#     # Make a temp wav file
+#     fd, wav_path = tempfile.mkstemp(suffix=".wav")
+#     os.close(fd)
 
-    try:
-        # Generate speech -> wav (piper reads text from stdin)
-        subprocess.run(
-            ["piper", "--model", PIPER_MODEL, "--output_file", wav_path],
-            input=text,
-            text=True,
-            check=True
-        )
+#     try:
+#         # Generate speech -> wav (piper reads text from stdin)
+#         subprocess.run(
+#             ["piper", "--model", PIPER_MODEL, "--output_file", wav_path],
+#             input=text,
+#             text=True,
+#             check=True
+#         )
 
-        # Play wav (ALSA)
-        subprocess.run(["aplay", wav_path], check=True)
+#         # Play wav (ALSA)
+#         subprocess.run(["aplay", wav_path], check=True)
 
-    finally:
-        # Clean up
-        if os.path.exists(wav_path):
-            os.remove(wav_path)
+#     finally:
+#         # Clean up
+#         if os.path.exists(wav_path):
+#             os.remove(wav_path)
 
-def speaker(queue: Queue):
-    while True:
-        item = queue.get()
-        if item is None:
-            queue.task_done()
-            break
+# def speaker(queue: Queue):
+#     while True:
+#         item = queue.get()
+#         if item is None:
+#             queue.task_done()
+#             break
 
-        text, delay = item
-        time.sleep(delay)
+#         text, delay = item
+#         time.sleep(delay)
 
-        speak_piper(text)
+#         speak_piper(text)
 
-        queue.task_done()
+#         queue.task_done()
 
-speech_queue = Queue()
+# speech_queue = Queue()
 
-speaker_thread = threading.Thread(target=speaker, args=(speech_queue,), daemon=True)
-speaker_thread.start()
+# speaker_thread = threading.Thread(target=speaker, args=(speech_queue,), daemon=True)
+# speaker_thread.start()
 
-lines = [
-    ("Hello I am PAUL the programmable autonomous utility lift. I am your personal assistant.", 0.1),
-]
+# lines = [
+#     ("Hello I am PAUL the programmable autonomous utility lift. I am your personal assistant.", 0.1),
+# ]
 
-for line in lines:
-    speech_queue.put(line)
+# for line in lines:
+#     speech_queue.put(line)
 
-speech_queue.join()
+# speech_queue.join()
 
-speech_queue.put(None)
-speech_queue.join()
+# speech_queue.put(None)
+# speech_queue.join()
 
-print("Done Speaking")
+# print("Done Speaking")
+
+# flite -voice slt "Write words here" output.wav
+# This voice is "good enough" 
+# Currerently it creates a .wav file, need to be able to play it via code.
 
